a61f71c1c9f0575b397b8bb8483f49876abfe2fa
Jorge-Matheus <crossfire_brasil2013@hotmail.com>
2024-08-05 16:48:01 -0300

    Process of listing registered users and converting to list of profiles

--- src/main/java/com/mballem/curso/security/web/controller/UsuarioController.java
+++ src/main/java/com/mballem/curso/security/web/controller/UsuarioController.java
@@ -1,15 +1,22 @@
 package com.mballem.curso.security.web.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.mballem.curso.security.domain.Usuario;
+import com.mballem.curso.security.service.UsuarioService;
 
 @Controller
 @RequestMapping("u")
 public class UsuarioController {
 
+	@Autowired
+	private UsuarioService service;
 	
 	// abrir cadastro de usuarios (medico/admin/paciente)
 	@GetMapping("/novo/cadastro/usuario")
@@ -17,5 +24,19 @@ public class UsuarioController {
 		
 		
 		return "usuario/cadastro";
+	}
+	
+	// abrir lista de usuarios
+	@GetMapping("/lista")
+	public String listarUsuarios() {
+		
+		return "usuario/lista";
+	}
+	
+	// lista de usuarios no datatables
+	@GetMapping("/datatables/server/usuarios")
+	public ResponseEntity<?> listarUsuariosDatatables(HttpServletRequest request) {
+		
+		return ResponseEntity.ok(service.buscarTodos(request));
 	}
 }
