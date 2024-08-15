7c281e3a60668d36fc1c4d59617a730803efe967
Jorge-Matheus <crossfire_brasil2013@hotmail.com>
2024-08-14 22:44:41 -0300

    Selecting query data and time and entering a query

--- src/main/java/com/mballem/curso/security/repository/AgendamentoRepository.java
+++ src/main/java/com/mballem/curso/security/repository/AgendamentoRepository.java
@@ -14,15 +14,16 @@ import com.mballem.curso.security.domain.Horario;
 public interface AgendamentoRepository extends JpaRepository<Agendamento, Long> {
 
 	@Query("select h "
-		     + "from Horario h "
-		     + "where not exists ("
-		     + "    select a.horario.id "
-		     + "    from Agendamento a "
-		     + "    where a.medico.id = :id "
-		     + "    and a.dataConsulta = :data "
-		     + "    and a.horario.id = h.id"
-		     + ") "
-		     + "order by h.horaMinuto asc")
+			+ "from Horario h "
+			+ "where not exists("
+				+ "select a.horario.id "
+					+ "from Agendamento a "
+					+ "where "
+						+ "a.medico.id = :id and "
+						+ "a.dataConsulta = :data and "
+						+ "a.horario.id = h.id "
+			+ ") "
+			+ "order by h.horaMinuto asc")
 	List<Horario> findByMedicoIdAndDataNotHorarioAgendado(Long id, LocalDate data);
 
 	
