159a1a75be26072348d7984754acc58818199e96
Jorge-Matheus <crossfire_brasil2013@hotmail.com>
2024-08-15 01:45:09 -0300

    Appointment History and Appointment History Projection

--- src/main/java/com/mballem/curso/security/repository/AgendamentoRepository.java
+++ src/main/java/com/mballem/curso/security/repository/AgendamentoRepository.java
@@ -3,12 +3,15 @@ package com.mballem.curso.security.repository;
 import java.time.LocalDate;
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.mballem.curso.security.domain.Agendamento;
 import com.mballem.curso.security.domain.Horario;
+import com.mballem.curso.security.repository.projection.HistoricoPaciente;
 
 @Repository
 public interface AgendamentoRepository extends JpaRepository<Agendamento, Long> {
@@ -25,6 +28,25 @@ public interface AgendamentoRepository extends JpaRepository<Agendamento, Long> 
 			+ ") "
 			+ "order by h.horaMinuto asc")
 	List<Horario> findByMedicoIdAndDataNotHorarioAgendado(Long id, LocalDate data);
+
+	@Query("select a.id as id,"
+			+ "a.paciente as paciente,"
+			+ "CONCAT(a.dataConsulta, ' ', a.horario.horaMinuto) as dataConsulta, "
+			+ "a.medico as medico,"
+			+ "a.especialidade as especialidade "
+			+ "from Agendamento a "
+			+ "where a.paciente.usuario.email like :email")
+	Page<HistoricoPaciente> findHistoricoByPacienteEmail(String email, Pageable pageable);
+
+	
+	@Query("select a.id as id,"
+			+ "a.paciente as paciente,"
+			+ "CONCAT(a.dataConsulta, ' ', a.horario.horaMinuto) as dataConsulta, "
+			+ "a.medico as medico,"
+			+ "a.especialidade as especialidade "
+			+ "from Agendamento a "
+			+ "where a.medico.usuario.email like :email")
+	Page<HistoricoPaciente> findHistoricoByMedicoEmail(String email, Pageable pageable);
 
 	
 }
--- src/main/java/com/mballem/curso/security/web/controller/AgendamentoController.java
+++ src/main/java/com/mballem/curso/security/web/controller/AgendamentoController.java
@@ -2,11 +2,14 @@ package com.mballem.curso.security.web.controller;
 
 import java.time.LocalDate;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.format.annotation.DateTimeFormat.ISO;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,6 +21,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import com.mballem.curso.security.domain.Agendamento;
 import com.mballem.curso.security.domain.Especialidade;
 import com.mballem.curso.security.domain.Paciente;
+import com.mballem.curso.security.domain.PerfilTipo;
 import com.mballem.curso.security.service.AgendamentoService;
 import com.mballem.curso.security.service.EspecialidadeService;
 import com.mballem.curso.security.service.PacienteService;
@@ -28,35 +32,58 @@ public class AgendamentoController {
 
 	@Autowired
 	private AgendamentoService service;
-	
+
 	@Autowired
 	private PacienteService pacienteService;
-	
+
 	@Autowired
 	private EspecialidadeService especialidadeService;
-	
-	@GetMapping({"/agendar"})
+
+	@GetMapping({ "/agendar" })
 	public String agendarConsulta(Agendamento agendamento) {
-		
+
 		return "agendamento/cadastro";
 	}
-	
+
+	// busca os horarios livres, ou seja, sem agendamento
 	@GetMapping("/horario/medico/{id}/data/{data}")
-	public ResponseEntity<?> getHorarios(@PathVariable("id") Long id, @PathVariable("data") @DateTimeFormat(iso = ISO.DATE) LocalDate data) {
-		return ResponseEntity.ok(service.buscarHorariosNaoAgendadosPorMedicoIdEData(id,data));
+	public ResponseEntity<?> getHorarios(@PathVariable("id") Long id,
+			@PathVariable("data") @DateTimeFormat(iso = ISO.DATE) LocalDate data) {
+		return ResponseEntity.ok(service.buscarHorariosNaoAgendadosPorMedicoIdEData(id, data));
 	}
-	
-	@PostMapping({"/salvar"})
+
+	// salvar uma consulta agendada
+	@PostMapping({ "/salvar" })
 	public String salvar(Agendamento agendamento, RedirectAttributes attr, @AuthenticationPrincipal User user) {
 		Paciente paciente = pacienteService.BuscarPoUsuarioEmail(user.getUsername());
 		String titulo = agendamento.getEspecialidade().getTitulo();
-		Especialidade especialidade = especialidadeService
-				.buscarPorTitulos(new String[] {titulo})
-				.stream().findFirst().get();
+		Especialidade especialidade = especialidadeService.buscarPorTitulos(new String[] { titulo }).stream()
+				.findFirst().get();
 		agendamento.setEspecialidade(especialidade);
 		agendamento.setPaciente(paciente);
 		service.salvar(agendamento);
 		attr.addFlashAttribute("sucesso", "Sua consulta foi agendada com sucesso.");
 		return "redirect:/agendamentos/agendar";
+	}
+
+	// abrir página de historico de agendamento do paciente
+	@GetMapping({ "/historico/paciente", "/historico/consultas" })
+	public String historico() {
+		return "agendamento/historico-paciente";
+	}
+
+	// localizar o historico de agendamentos por usuario logado
+	@GetMapping("/datatables/server/historico")
+	public ResponseEntity<?> historicoAgendamentosPorPaciente(HttpServletRequest request,
+			@AuthenticationPrincipal User user) {
+		if (user.getAuthorities().contains(new SimpleGrantedAuthority(PerfilTipo.PACIENTE.getDesc()))) {
+			return ResponseEntity.ok(service.buscarHistoricoPorPacienteEmail(user.getUsername(), request));
+		}
+
+		if (user.getAuthorities().contains(new SimpleGrantedAuthority(PerfilTipo.MEDICO.getDesc()))) {
+			return ResponseEntity.ok(service.buscarHistoricoPorMedicoEmail(user.getUsername(), request));
+		}
+
+		return ResponseEntity.notFound().build();
 	}
 }
