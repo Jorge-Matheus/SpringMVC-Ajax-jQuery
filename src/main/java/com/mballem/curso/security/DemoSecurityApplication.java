e9bf6ac02cb86fe8e098a3a89e2338fe8a60bd51
Jorge-Matheus <crossfire_brasil2013@hotmail.com>
2024-08-23 04:48:34 -0300

    Receiving the registration confirmation request

--- src/main/java/com/mballem/curso/security/DemoSecurityApplication.java
+++ src/main/java/com/mballem/curso/security/DemoSecurityApplication.java
@@ -1,32 +1,26 @@
 package com.mballem.curso.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
-
-import com.mballem.curso.security.service.EmailService;
 
 @SpringBootApplication
-public class DemoSecurityApplication implements CommandLineRunner {
+public class DemoSecurityApplication {
 
 	public static void main(String[] args) {
 		//System.out.println(new BCryptPasswordEncoder().encode("123456"));
 		SpringApplication.run(DemoSecurityApplication.class, args);
 	}
 	
-	@Autowired
-	JavaMailSender sender;
+	//@Autowired
+	//JavaMailSender sender;
 	
-	@Autowired
-	EmailService emailService;
+	//@Autowired
+	//EmailService emailService;
 
-	@Override
-	public void run(String... args) throws Exception {
-		emailService.enviarPedidoDeConfirmacaoDeCadastro("jacob.jacobjazz@gmail.com", "9852pol");
-	}
+	//@Override
+	//public void run(String... args) throws Exception {
+		//emailService.enviarPedidoDeConfirmacaoDeCadastro("jacob.jacobjazz@gmail.com", "9852pol");
+	//}
 	
 
 }
